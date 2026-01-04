0a23d396e1d476096147dc04ce2310ae572d63c5
pronelclickbank-lgtm <pronelclickbank@gmail.com>
2026-01-04 11:03:25 +0530

    Simplify main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,6 @@
 
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
